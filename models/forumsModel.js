1a363ec12dbc61d28ba7c7fd608743e819948b50
krulex98 <krulex98@gmail.com>
2020-01-06 16:54:52 +0300

    forum returns username as user

--- models/forumsModel.js
+++ models/forumsModel.js
@@ -7,13 +7,14 @@ class forumsModel {
 		try {
 			await db.one(`SELECT * FROM users WHERE nickname = '${forum.user}'`);
 			return await db.one('INSERT INTO forums(slug, title, username) ' +
-				'VALUES(${forum.slug}, ${forum.title}, ${forum.user}) RETURNING *', {forum: forum});
+				'VALUES(${forum.slug}, ${forum.title}, ${forum.user}) RETURNING slug, posts, threads, title, username as user', {forum: forum});
 		} catch (error) {
 			if (error instanceof pgp.errors.QueryResultError) {
 				throw new errors.NotFoundError();
 			}
 
-			const forums = await db.many('SELECT * FROM forums WHERE slug = ${forum.slug}', {forum: forum});
+			const forums = await db.many('SELECT slug, posts, threads, title, username as user FROM forums WHERE slug = ${forum.slug}',
+				{forum: forum});
 			throw new errors.AlreadyExistsError(forums);
 		}
 	}
